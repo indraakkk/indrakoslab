e738970d189c6968be67fc6649ba04825bb1d2c9
indraakkk <indrakoslab@gmail.com>
2024-07-03 17:33:19 +0700

    feat: :sparkles: dark and light mode

--- apps/web/components/Navbar.vue
+++ apps/web/components/Navbar.vue
@@ -1,8 +1,31 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+const theme = ref({
+  dark() {
+    colorMode.preference = 'dark';
+  },
+  light() {
+    colorMode.preference = 'light';
+  },
+  active: false,
+});
+
+const handleChange = (state: boolean) => {
+  if (state) {
+    theme.value.light();
+    theme.value.active = true;
+  } else {
+    theme.value.dark();
+    theme.value.active = false;
+  }
+};
+</script>
+
 <template>
   <header>
     <div class="flex gap-3 justify-center">
       <div
-        class="flex border border-cyan-500 shadow-md rounded-full p-3 m-3 gap-3"
+        class="flex border border-white shadow-md rounded-full px-6 py-3 m-3 gap-3"
       >
         <p>indrakoslab</p>
         <span>|</span>
@@ -13,6 +36,10 @@
             <li>Blog</li>
           </ul>
         </nav>
+        <span>|</span>
+        <p>Dark</p>
+        <Switch :checked="theme.active" @update:checked="handleChange" />
+        <p>Light</p>
       </div>
     </div>
   </header>
--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,17 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
   components: [
     {
       path: '~/components',
       pathPrefix: false,
     },
   ],
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode',
+    'shadcn-nuxt',
+    '@nuxtjs/google-fonts',
+  ],
+  googleFonts: {
+    families: {
+      Inter: {
+        wght: '200..900',
+        ital: '200..700',
+      },
+    },
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui',
+  },
+  colorMode: {
+    classSuffix: '',
+  },
 });
